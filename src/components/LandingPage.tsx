a61fc20f67c3dd363ae6617064b960567fa5902a
Paula-Wang <nimopaula8@gmail.com>
2024-04-13 13:54:33 +0300

    refactor: :recycle: change code from grid system to flexbox to provide spacing

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,7 +3,7 @@ import AppBar from './AppBar';
 import Drawer from './Drawer';
 import EnvironmentalSectionCard from './EnvironmentalSectionCard';
 import SoilHealthSectionCard from './SoilHealthSectionCard';
-import { Grid } from '@mui/material';
+// import { Grid } from '@mui/material';
 import WaterSupplySectionCard from './WaterSupplySectionCard';
 
 const LandingPage = () => {
@@ -11,20 +11,12 @@ const LandingPage = () => {
     <div>
       <AppBar />
       <Drawer />
-      <main style={{ padding: '2rem' }}>
-        <Grid container spacing={2}> {/* Add spacing between cards */}
-          <Grid item xs={12} md={6}>
-            <EnvironmentalSectionCard />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <SoilHealthSectionCard />
-          </Grid>
-        </Grid>
-        <Grid item xs={12} md={6}>
-            <WaterSupplySectionCard />
-          </Grid>
-        {/* Add more content sections here */}
-      </main>
+      <main style={{ padding: '2rem', display: 'flex', justifyContent: 'space-between' }}>
+  <EnvironmentalSectionCard />
+  <SoilHealthSectionCard />
+  <WaterSupplySectionCard />
+  {/* Add more content sections here */}
+</main>
     </div>
   );
 };
